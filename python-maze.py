dae53fe66ae88121472712ad7f30c7afa2ac10a0
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-11-21 09:29:46 +0100

    edit game color styles for turn left

--- python-maze.py
+++ python-maze.py
@@ -9,7 +9,7 @@ colorama.init(autoreset=True)
 
 def play_adventure(route, adventure_questions):
     
-    chance_list = ["AL"] * 5 + ["FW"] * 10 + ["Q"] * 45
+    chance_list = ["AL"] * 59 + ["FW"] * 10 + ["Q"] * 45
     adventure_questions = ad[:]
     
     
@@ -49,14 +49,14 @@ def play_adventure(route, adventure_questions):
         else:
             print()  
         time.sleep(0.5)
-        print(f"{current_route_questions}\n  {Style.DIM}{Fore.YELLOW} {quit_game}")
+        print(f"{Style.BRIGHT}{Fore.YELLOW}{current_route_questions}\n{Style.DIM}{Fore.LIGHTYELLOW_EX}{quit_game}")
         time.sleep(0.5)
         answer = input().lower()
         if answer.lower() == "q":
             print()
             time.sleep(0.5)
             life = 0
-            print(f"You covered {distance} meters before Python ate you.")
+            print(f"{Style.BRIGHT}{Fore.YELLOW}You covered {distance} meters before Python ate you.")
             break
         else:
             
@@ -80,20 +80,20 @@ def play_adventure(route, adventure_questions):
                 if answer in ['left', 'l', 'L'] and  way_to_left == "AL":
                     print()
                     time.sleep(0.5)
-                    print("You chose the right path and found an extra life.")
+                    print(f"{Style.BRIGHT}{Fore.GREEN}You chose the right path and found an extra life.")
                     time.sleep(0.5)
                     life = life + 1
-                    print(f"Now you have {life} lifes")
+                    print(f"{Style.BRIGHT}{Fore.YELLOW}Now you have {Style.BRIGHT}{Fore.GREEN}{life}{Style.BRIGHT}{Fore.YELLOW} lifes")
                     time.sleep(0.5)
                     distance = distance + 100
-                    print(f"You have advanced {distance} meters deeper into the Python Maze")
+                    print(f"{Style.BRIGHT}{Fore.YELLOW}You have advanced {Style.BRIGHT}{Fore.GREEN}{distance}{Style.BRIGHT}{Fore.YELLOW} meters deeper into the Python Maze")
                     print()
                     time.sleep(0.5)
-                    print("other paths led you:") 
+                    print(f"{Style.BRIGHT}{Fore.WHITE}other paths led you:") 
                     time.sleep(0.5)   
-                    print(f"right to {"extra life" if way_to_right == "AL" else "free path" if way_to_right == "FW" else "battle with the python"}")  
+                    print(f"{Style.BRIGHT}{Fore.YELLOW}right to {(Style.BRIGHT + Fore.GREEN + 'extra life') if way_to_right == "AL" else (Style.BRIGHT + Fore.YELLOW + 'free path') if way_to_right == "FW" else (Style.BRIGHT + Fore.RED + 'battle with the python')}")  
                     time.sleep(0.5)
-                    print(f"straight to {"extra life" if way_to_straight == "AL" else "free path" if way_to_straight == "FW" else "battle with the python"}")
+                    print(f"{Style.BRIGHT}{Fore.YELLOW}straight to {(Style.BRIGHT + Fore.GREEN + 'extra life') if way_to_straight == "AL" else (Style.BRIGHT + Fore.YELLOW + 'free path') if way_to_straight == "FW" else (Style.BRIGHT + Fore.RED + 'battle with the python')}")
                     time.sleep(0.5)
                     
                 elif answer in ['left', 'l', 'L'] and  way_to_left == "FW":
