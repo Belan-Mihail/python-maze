05c21a9fd19a46124d828d3d357001a94c715464
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-11-23 22:23:40 +0100

    add color to the name of game

--- graph.py
+++ graph.py
@@ -1,31 +1,25 @@
 import time
+import colorama
+
+
+from colorama import Fore, Style
+colorama.init(autoreset=True)
 def print_game_name():
     time.sleep(0.5)
-    print("______      _   _                 _       _           _                _       _   _    ")
+    print(f"{Style.BRIGHT}{Fore.GREEN}______      _   _                 _      {Fore.RED} _           _                _       _   _    ")
     time.sleep(0.5)
-    print("| ___ \    | | | |               ( )     | |         | |              (_)     | | | |   ")
+    print(f"{Style.BRIGHT}{Fore.GREEN}| ___ \    | | | |               ( )     {Fore.RED}| |         | |              (_)     | | | |   ")
     time.sleep(0.5)
-    print("| |_/ /   _| |_| |__   ___  _ __ |/ ___  | |     __ _| |__  _   _ _ __ _ _ __ | |_| |__  ")
+    print(f"{Style.BRIGHT}{Fore.GREEN}| |_/ /   _| |_| |__   ___  _ __ |/ ___  {Fore.RED}| |     __ _| |__  _   _ _ __ _ _ __ | |_| |__  ")
     time.sleep(0.5)
-    print("|  __/ | | | __| '_ \ / _ \| '_ \  / __| | |    / _` | '_ \| | | | '__| | '_ \| __| '_ \ ")
+    print(f"{Style.BRIGHT}{Fore.GREEN}|  __/ | | | __| '_ \ / _ \| '_ \  / __| {Fore.RED}| |    / _` | '_ \| | | | '__| | '_ \| __| '_ \ ")
     time.sleep(0.5)
-    print("| |  | |_| | |_| | | | (_) | | | | \__ \ | |___| (_| | |_) | |_| | |  | | | | | |_| | | |")
+    print(f"{Style.BRIGHT}{Fore.GREEN}| |  | |_| | |_| | | | (_) | | | | \__ \ {Fore.RED}| |___| (_| | |_) | |_| | |  | | | | | |_| | | |")
     time.sleep(0.5)
-    print("\_|   \__, |\__|_| |_|\___/|_| |_| |___/ \_____/\__,_|_.__/ \__, |_|  |_|_| |_|\__|_| |_|")
+    print(f"{Style.BRIGHT}{Fore.GREEN}\_|   \__, |\__|_| |_|\___/|_| |_| |___/ {Fore.RED}\_____/\__,_|_.__/ \__, |_|  |_|_| |_|\__|_| |_|")
     time.sleep(0.5)
-    print("       __/ |                                                 __/ |                       ")
+    print(f"{Style.BRIGHT}{Fore.GREEN}       __/ |                                 {Fore.RED}                __/ |                       ")
     time.sleep(0.5)
-    print("      |___/                                                 |___/            ")
+    print(f"{Style.BRIGHT}{Fore.GREEN}      |___/                                        {Fore.RED}         |___/            ")
     time.sleep(0.5)
 
-
-text = """
-______      _   _                 _       _           _                _       _   _     
-| ___ \    | | | |               ( )     | |         | |              (_)     | | | |    
-| |_/ /   _| |_| |__   ___  _ __ |/ ___  | |     __ _| |__  _   _ _ __ _ _ __ | |_| |__  
-|  __/ | | | __| '_ \ / _ \| '_ \  / __| | |    / _` | '_ \| | | | '__| | '_ \| __| '_ \ 
-| |  | |_| | |_| | | | (_) | | | | \__ \ | |___| (_| | |_) | |_| | |  | | | | | |_| | | |
-\_|   \__, |\__|_| |_|\___/|_| |_| |___/ \_____/\__,_|_.__/ \__, |_|  |_|_| |_|\__|_| |_|
-       __/ |                                                 __/ |                       
-      |___/                                                 |___/                                            
-"""
